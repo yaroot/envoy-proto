43171552094179be4e2a1bd7ce3a70c8a450b1b7
Yan Su <tsu@yaroot.net>
2022-08-05 17:15:09 +0900

    .

--- build.sh
+++ build.sh
@@ -1,16 +1,43 @@
-#!/bin/bash
+#!/bin/zsh
+
+set -x
 
 pushd deps
 
-git clone --depth=1 https://github.com/envoyproxy/data-plane-api.git
-git clone --depth=1 https://github.com/googleapis/googleapis.git
-git clone --depth=1 https://github.com/grafeas/grafeas.git
-git clone --depth=1 https://github.com/census-instrumentation/opencensus-proto.git
-git clone --depth=1 https://github.com/open-telemetry/opentelemetry-proto.git
-git clone --depth=1 https://github.com/envoyproxy/protoc-gen-validate.git
-git clone --depth=1 https://github.com/cncf/udpa.git
-git clone --depth=1 https://github.com/cncf/xds.git
+
+git clone --depth=1 https://github.com/protocolbuffers/protobuf
+git clone --depth=1 https://github.com/census-instrumentation/opencensus-proto
+git clone --depth=1 https://github.com/cncf/udpa
+git clone --depth=1 https://github.com/cncf/xds
+git clone --depth=1 https://github.com/envoyproxy/data-plane-api
+git clone --depth=1 https://github.com/envoyproxy/protoc-gen-validate
+git clone --depth=1 https://github.com/googleapis/googleapis
+git clone --depth=1 https://github.com/grafeas/grafeas
+git clone --depth=1 https://github.com/open-telemetry/opentelemetry-proto
+git clone --depth=1 https://github.com/prometheus/client_model
+
 
 popd
 
+
+protoc \
+  -Ideps/protobuf/src \
+  -Ideps/data-plane-api \
+  -Ideps/googleapis \
+  -Ideps/grafeas \
+  -Ideps/opencensus-proto/src \
+  -Ideps/opentelemetry-proto \
+  -Ideps/protoc-gen-validate \
+  -Ideps/xds \
+  -Ideps/client_model \
+  --python_out=gen/python \
+  deps/data-plane-api/envoy/**/*.proto \
+  deps/client_model/io/**/*.proto \
+  deps/googleapis/google/**/*.proto \
+  deps/googleapis/grafeas/**/*.proto \
+  deps/opencensus-proto/src/opencensus/proto/**/*.proto \
+  deps/opentelemetry-proto/opentelemetry/proto/**/*.proto \
+  deps/protoc-gen-validate/validate/*.proto \
+  deps/xds/udpa/**/*.proto \
+  deps/xds/xds/**/*.proto
 
